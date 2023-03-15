ea78c16778456d44eb41ed4902c8a5b644f18db7
Thiago Colombiano <82493225+tcolombiano@users.noreply.github.com>
2023-03-15 20:59:38 +0000

    Initial commit

---
+++ Arquivo teste/testejigoku.c
@@ -0,0 +1,11 @@
+//esse vai ser o arquivo que vou usar para testar o projeto escola enquanto não aprendo a atualizar diretamente.
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+
+
+
+  
+}
